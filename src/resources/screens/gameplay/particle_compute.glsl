96898eceb808fbc63002b9f4e229dc901cb3f5b0
Aleksei Chistov <aleksey.chistov@gmail.com>
2024-07-28 15:40:50 +0500

    docs

--- src/resources/screens/gameplay/particle_compute.glsl
+++ src/resources/screens/gameplay/particle_compute.glsl
@@ -1,3 +1,8 @@
+//
+// NOTE: Оставил комменты из примера.
+// ref: https://github.com/arceryz/raylib-gpu-particles/blob/master/Shaders/particle_compute.glsl
+//
+
 // Version 430 supports compute shaders.
 #version 430
 
@@ -18,7 +23,6 @@ layout (local_size_x = 1024, local_size_y = 1, local_size_z = 1) in;
 //
 layout(std430, binding=0) buffer ssbo0 { vec4 positions[]; };
 layout(std430, binding=1) buffer ssbo1 { vec4 velocities[]; };
-// layout(std430, binding=2) buffer ssbo2 { float timesOfCreation[]; };
 
 // Uniform values are the way in which we can modify the shader efficiently.
 // These can be updated every frame efficiently.
@@ -30,5 +34,4 @@ layout(location=2) uniform float dt;
 void main() {
     uint index = gl_GlobalInvocationID.x;
     positions[index].xyz   += velocities[index].xyz * (dt * timeScale);
-    // timesOfCreation[index] += dt;
 }
